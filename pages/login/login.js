c73b5b29a91311dc7d3e04524e3483aceaab9a0c
zzw-111-bit <72742031+zzw-111-bit@users.noreply.github.com>
2021-10-24 18:53:05 +0800

    Add files via upload

---
+++ pages/login/login.js
@@ -0,0 +1,13 @@
+// pages/login/index.js
+Page({
+  data: {
+
+  },
+  handleGetUserInfo(e){
+      const {userInfo}=e.detail;
+      wx.setStorageSync('userinfo', userInfo);
+      wx.navigateBack({
+        delta: 1,
+      })
+  }
+})
